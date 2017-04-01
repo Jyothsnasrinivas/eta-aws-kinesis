9444fbdebbfde69678f2d80be5bb2a64fe86043c
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-04-02 02:02:08 +0530

    Fixed errors in Kinesis.Waiters

--- src/AWS/Kinesis/Waiters.hs
+++ src/AWS/Kinesis/Waiters.hs
@@ -1,6 +1,9 @@
-module AWS.Kinesis.Waiters
+{-# LANGUAGE TypeOperators #-}
+module AWS.Kinesis.Waiters where
 
+import Java
 import AWS.Core
+import AWS.Kinesis.Model
 import Java.Collections
 import Java.NIO
 
@@ -9,4 +12,8 @@ import Java.NIO
 data {-# CLASS "com.amazonaws.services.kinesis.waiters.AmazonKinesisWaiters" #-} AmazonKinesisWaiters = AmazonKinesisWaiters (Object# AmazonKinesisWaiters)
   deriving Class
 
-foreign import java unsafe clearTagsEntries :: Java AddTagsToStreamRequest AddTagsToStreamRequest
+foreign import java unsafe shutdown :: Java AmazonKinesisWaiters ()
+
+foreign import java unsafe streamExists :: Java AmazonKinesisWaiters (Waiter DescribeStreamRequest)
+
+-- End com.amazonaws.services.kinesis.waiters.AmazonKinesisWaiters
